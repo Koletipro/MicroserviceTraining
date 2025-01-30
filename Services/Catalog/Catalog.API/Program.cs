bcacb848865ff3201be6837a7b58645645233c81
koletipro@gmail.com <koletipro@gmail.com>
2025-01-30 09:15:07 +0100

    Add docker compose

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
 builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddCarter();
